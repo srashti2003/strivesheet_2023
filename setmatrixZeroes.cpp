39aa36c54acc84c8eb590583e78a1a3402940b31
Srashti Gupta <109365275+srashti2003@users.noreply.github.com>
2023-06-14 22:31:25 +0530

    Update setmatrixZeroes.cpp

--- setmatrixZeroes.cpp
+++ setmatrixZeroes.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        int col[m] = {0};
-        int row[n] = {0};
+        int n=matrix.size();
+        int m =matrix[0].size();
+        set<int> x,y;
         for(int i=0; i<n;i++){           
             for(int j=0; j<m;j++){
                 if(matrix[i][j]==0) {
-                    row[i] = 1;
-                    col[j] = 1;
+                    x.insert(i);
+                    y.insert(j);
                 }
             }
         }
 
-        for(int i=0; i<matrix.size();i++){           
-            for(int j=0; j<matrix[i].size();j++){
-                if(row[i] || col[j]) {
+        for(int i=0; i<n;i++){           
+            for(int j=0; j<m;j++){
+                if(x.count(i)==1 || y.count(j)==1) {
                     matrix[i][j]=0;
                 }
             }
