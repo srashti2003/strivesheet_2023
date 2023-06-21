60709439414758f018f13db1d37e4bb205796394
Srashti Gupta <109365275+srashti2003@users.noreply.github.com>
2023-06-22 00:36:06 +0530

    Create mergeSortedArray.cpp

---
+++ mergeSortedArray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m-1;
+        int j = 0;
+        while(i>=0 && j<n){
+            if(nums1[i]>nums2[j]){
+                swap(nums1[i],nums2[j]);
+                i--;
+                j++;
+            }
+            else{
+                break;
+            }
+        }
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+    }
+};
