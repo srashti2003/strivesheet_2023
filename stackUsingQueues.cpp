02e111c82e223736b51ef840d76fe145a786c375
Srashti Gupta <109365275+srashti2003@users.noreply.github.com>
2023-07-20 22:00:21 +0530

    Create stackUsingQueues.cpp

---
+++ stackUsingQueues.cpp
@@ -0,0 +1,30 @@
+class MyStack {
+public:
+    queue<int> q;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q.push(x);
+        int n = q.size();
+        for(int i=0;i<n-1;i++){
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int result = q.front();
+        q.pop();
+        return result;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
